01a0eefffef0d001c1ab3359e06b2c03b11e9a00
Sai Sankeerth <sankeerth2292@gmail.com>
2019-09-08 20:19:53 +0530

    Login Required Decorator & User authentication lesson commit

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 from .models import PostModel
 
+@login_required(login_url='/login/')
 def post_model_list_view(request):
     qs = PostModel.objects.all()
     print(qs)
@@ -13,4 +15,7 @@ def post_model_list_view(request):
     context_dict = {
         'object_list': qs,
     }
+    # Using request.user.is_authenticated() --> we can check if user is authenticated
+    # and make changes as we deem fit
+    # for example, load different templates for authenticated & un-authenticated users
     return render(request=request, template_name=template_path, context=context_dict)
